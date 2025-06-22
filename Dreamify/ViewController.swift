b04751a756cbc6e808af3710b35a5977f52c9b98
vladyslavyatsuta <vy2000eng@gmail.com>
2025-06-22 13:22:47 -0400

    Initial Commit

---
+++ Dreamify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dreamify
+//
+//  Created by Vladyslav Yatsuta on 6/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DreamifyTests/DreamifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamifyTests.swift
+//  DreamifyTests
+//
+//  Created by Vladyslav Yatsuta on 6/22/25.
+//
+
+import Testing
+@testable import Dreamify
+
+struct DreamifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
